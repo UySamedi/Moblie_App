9c1208143e99acb02c20043fe3ac8d48aa007c00
UySamedi <uysamedi693@gmail.com>
2025-03-12 11:32:56 +0700

    add

--- app/src/main/java/com/example/my_app/HomeActivity.java
+++ app/src/main/java/com/example/my_app/HomeActivity.java
@@ -8,6 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
@@ -15,6 +21,15 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+
+        ImageSlider imageSlider = findViewById(R.id.image_Slider);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.fe_poster1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.fe_poster2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.fe_poster3, ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
